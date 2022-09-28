65abf786fe81bd78abd51ef8b44698e0eb09e35a
Anusha Samarabandu <asamara5@uwo.ca>
2022-09-28 15:06:21 -0400

    finished unit 2

---
+++ Create-With-Code-2/Prototype 2/Assets/Scripts/SphereManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereManager : MonoBehaviour
+{
+    public GameObject sphere;
+    private float spawnRangeX = 10;
+    private float spawnPosZ = -7.8f;
+    private float startDelay = 2;
+    private float spawnInterval = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnSphere", startDelay, spawnInterval);
+    }
+
+    void SpawnSphere()
+    {
+        if(GameObject.FindGameObjectsWithTag("Sphere").Length < 1)
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            Instantiate(sphere, spawnPos, sphere.transform.rotation);
+        }
+        
+    }
+}
---
+++ Create-With-Code-2/Prototype 2/Assets/Scripts/SpherePowerUp.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpherePowerUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        Debug.Log("Hit");
+        if (other.gameObject.tag == "Player")
+        {
+            GameObject[] animals = GameObject.FindGameObjectsWithTag("Animal");
+            foreach (GameObject a in animals)
+            {
+                GameObject.Destroy(a);
+            }
+            Destroy(gameObject);
+        }
+    }
+}
